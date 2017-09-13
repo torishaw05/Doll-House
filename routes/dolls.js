34e4a1dcecc8608fcae0e2c3dcc423a801bb02a4
Tori Shaw <torishaw@Toris-MacBook-Air.local>
2017-09-13 08:21:22 -0400

    Fixed

--- routes/dolls.js
+++ routes/dolls.js
@@ -1,10 +1,10 @@
 
 const express     = require("express");
-const Dolls        = require("../collections/dolls");
+const Dolls        = require("../models/dolls");
 const router      = express.Router();
 let results= [];
 
-// router.get("/", function(req, res) {
+// router.get("/", function(req, res)
 
 function findDolls(req, res, next) {
   Dolls.find({})
@@ -20,7 +20,7 @@ router.get("/", findDolls, function(req, res) {
   res.render("dolledit", {dolls: results});
 });
 router.post("/", function(req, res) {
-  Dolls.create({
+  Dolls.create ({
     inches: req.body.inches,
     material: req.body.material,
     color: req.body.color,
@@ -34,5 +34,82 @@ router.post("/", function(req, res) {
     console.log(err);
   })
   res.redirect("/");
+// });
+// router.post('/trash/:id', function(req, res) {
+// let id = req.params.id;
+//
+//   Dolls.DeleteOne({_id: id})
+//   .then(function (data) {
+//     res.redirect('/')
+//   })
+//   .catch(function(err) {
+//     res.send(err);
+//   })
+});router.get('/edit/:id' ,function (req, res) {
+
+  Dolls.find({_id: req.params.id})
+  .then(function(doll) {
+  res.render('edit', {doll: dolls});
+})
+  .catch(function(err) {
+    res.send(err);
+  });
+// });
+//
+//
+// router.post('/edit/:id', function(req, res)
+// let DesignDoll = req.params.id
+//
+//   Dolls.update({id: DesignDoll}, {
+//        inches: req.body.inches,
+//         material: req.body.material,
+//         color: req.body.color,
+//         brand: req.body.brand,
+//         history: req.body.history,
+//       })
+//
+ // );
+router.get("/trash/:id", function(req, res) {
+  models.User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(function(data) {
+      res.redirect("/");
+    });
 });
+
+// router.post("/edit/:id", function(req, res) {
+//   models.User.update({
+//     inches: req.body.inches,
+//     material: req.body.material,
+//     color: req.body.color,
+//     brand: req.body.brand,
+//     history: req.body.history,
+//   }, {
+//     where: {
+//       id: req.params.id
+//     }
+//   })
+//     .then(function(data) {
+//       res.redirect("/");
+//     });
+// });
+
+router.post("/", function(req, res) {
+  models.User.create({
+    inches: req.body.inches,
+    material: req.body.material,
+    color: req.body.color,
+    brand: req.body.brand,
+    history: req.body.history,
+  })
+  .then(function(data) {
+    res.redirect("/");
+  });
+
+});
+});
+
 module.exports = router;
